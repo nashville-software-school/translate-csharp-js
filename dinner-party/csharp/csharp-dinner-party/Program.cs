4f8d9511866b83964565c16e1ea036eaa18be568
calvincurry22 <calvincurry22@gmail.com>
2020-05-26 14:31:30 -0500

    Converted C# logic to JS logic for the places-names exercise

---
+++ dinner-party/csharp/csharp-dinner-party/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace csharp_dinner_party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Dictionary<string, int> bowlingScores = new Dictionary<string, int>()
+            { { "Bubba", 40 }, { "Louis", 120 }, { "Mary Jane", 200 }
+            };
+            Console.WriteLine(bowlingScores);
+        }
+    }
+}
--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,6 +1,25 @@
 function main() {
   // Put your code here
+  const names = [
+    "Nashville", "Hong Kong", "The back yard", "Earth",
+    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
+  ]
 
+  console.log(" All Places Names")
+
+  for (name of names) {
+    console.log(name)
+  }
+
+  console.log()
+
+  const theNames = names.filter(name => name.startsWith("The"))
+
+  console.log("'The' Place Names")
+
+  for (name of theNames) {
+    console.log(name)
+  }
 }
 
 main();
