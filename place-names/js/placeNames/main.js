23979791d6ed27cbace71fd8d2fd8967e3b44aae
Brant <brant.pippin@gmail.com>
2020-05-26 13:43:34 -0500

    translated C# code into Javascript code to console an filter place names.

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,6 +1,21 @@
 function main() {
   // Put your code here
+  const places = ["Nashville", "Hong Kong", "The back yard", "Earth", "London", "The mall", "Ryman Auditorium", "The Great Wall of China"]
+  
+  console.log("All Place Names")
 
+  for (place of places) {
+    console.log(place)
+  }
+
+  console.log(" ")
+
+  const filtPlaces = places.filter(fPlace => fPlace.startsWith("The"))
+
+  console.log("'The' Place Names")
+  for (place of filtPlaces) {
+    console.log(place)
+  }
 }
 
 main();
