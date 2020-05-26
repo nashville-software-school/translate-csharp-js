61868c18f97b9d3522893e5b8750139a0a0205a5
ThomasELong <thomaselong12@gmail.com>
2020-05-26 14:11:53 -0500

    place-names done

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,5 +1,28 @@
 function main() {
   // Put your code here
+const names = [
+  "Nashville", "Hong Kong", "The back yard", "Earth",
+  "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
+]
+
+
+
+
+
+
+
+
+console.log("All Place Names")
+for (name of names) {
+  console.log(name)
+}
+console.log(" ")
+
+console.log("'The' Place Names")
+const theNames = names.filter(tn => tn.startsWith("The"))
+for (name of theNames) {
+  console.log(name)
+}
 
 }
 
