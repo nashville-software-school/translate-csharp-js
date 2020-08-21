858e2ba3d841e473d2c0d733c9db739ce29e6dad
Patrick Murphy <engine_ear@mac.com>
2020-08-21 15:22:29 -0500

    rolling dice complete

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,6 +1,20 @@
 function main() {
   // Put your code here
+  const roll = () => {
+    return Math.floor(Math.random() * 6 + 1);
+  };
+  console.log("Let's roll some dice, baby!");
+  console.log("---------------------------");
 
+  for (let i = 0; i < 10; i++) {
+    let die1 = roll();
+    let die2 = roll();
+    let message = `${die1} + ${die2} === ${die1 + die2}`;
+    if (die1 === die2) {
+      message += " DOUBLES!";
+    }
+    console.log(message);
+  }
 }
 
 main();
