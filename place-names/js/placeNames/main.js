33c686684dc5f2c262ca53fc100078b7b2e5b40d
Patrick Murphy <engine_ear@mac.com>
2020-08-21 14:20:11 -0500

    Place-names

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,6 +1,29 @@
 function main() {
   // Put your code here
+  const names = [
+    "Nashville",
+    "Hong Kong",
+    "The back yard",
+    "Earth",
+    "London",
+    "The mall",
+    "Ryman Auditorium",
+    "The Great Wall of China",
+  ];
 
+  console.log("All Place Names");
+
+  names.forEach((name) => {
+    console.log(name);
+  });
+
+  console.log("");
+  const theNames = names.filter((n) => {
+    return n.startsWith("The");
+  });
+  console.log("'The' Place Names");
+  theNames.forEach((name) => {
+    console.log(name)
+  })
 }
-
 main();
