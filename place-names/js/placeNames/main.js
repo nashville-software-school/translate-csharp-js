717ad8151b88d69d3e12f9386a9b75c8aabfbb5c
Kelley Crittenden <kelleylcrittenden@gmail.com>
2020-08-23 16:38:54 -0500

    place names lesson complete

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,6 +1,17 @@
 function main() {
   // Put your code here
+  const names = ["Nashville", "Hong Kong", "The back yard", "Earth",
+    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"]
 
+  for (let name of names) {
+    console.log(name)
+  }
+
+  const listNames = names
+  for (name of listNames) {
+    if (name.startsWith("The") === true)
+      console.log(name)
+  }
 }
 
 main();
