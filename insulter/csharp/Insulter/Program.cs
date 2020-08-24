117dce6a1e3b688177bc030f158d686be5641253
David Larsen <davidplarsen@gmail.com>
2020-08-24 11:54:38 -0500

    Initialize c-sharp app

---
+++ insulter/csharp/Insulter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Insulter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
