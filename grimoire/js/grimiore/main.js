edae0a5bfe39ad995d4c8b476a2cf0cc800e3c7b
Patrick Murphy <engine_ear@mac.com>
2020-08-23 20:13:35 -0500

    Grimiore

--- grimoire/js/grimiore/main.js
+++ grimoire/js/grimiore/main.js
@@ -1,6 +1,64 @@
 function main() {
   // Put your code here
+  const allSpells = [
+    {
+      name: "Turn enemy into a newt",
+      IsEvil: true,
+      EnergyRequired: 5.1,
+    },
+    {
+      name: "Conjure some gold for a local charity",
+      IsEvil: false,
+      EnergyRequired: 2.99,
+    },
+    {
+      name: "Give a deaf person the ability to heal",
+      IsEvil: false,
+      EnergyRequired: 12.2,
+    },
+    {
+      name: "Make yourself emperor of the universe",
+      IsEvil: true,
+      EnergyRequired: 100,
+    },
+    {
+      name: "Convince your relatives your political views are correct",
+      IsEvil: false,
+      EnergyRequired: 2921.5,
+    },
+  ];
 
+  const SpellBook = {
+    makeEvilSpellBook() {
+      let evilSpells = allSpells.filter((spell) => {
+        if (spell.IsEvil) {
+          return spell;
+        }
+      });
+      return evilSpells;
+    },
+    makeGoodSpellBook() {
+      let goodSpells = allSpells.filter((spell) => {
+        if (!spell.IsEvil) {
+          return spell;
+        }
+      });
+      return goodSpells;
+    },
+  };
+  let evilSpells = SpellBook.makeEvilSpellBook();
+  let goodSpells = SpellBook.makeGoodSpellBook();
+  console.log("Do you believe in magic?");
+  console.log("------------------------");
+  console.log("Good Book");
+  goodSpells.forEach((spell) => {
+    console.log(spell.name);
+  });
+  console.log("");
+  console.log("Evil Book");
+  evilSpells.forEach((spell) => {
+    console.log(spell.name);
+  });
 }
 
 main();
