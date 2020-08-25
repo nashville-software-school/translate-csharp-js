96c6989f3b229f9f68c2c02072f3caae4efe9325
Jon Riley <jonrrile@ut.utm.edu>
2020-08-25 10:30:12 -0500

    Fizzbuzz complete

---
+++ fizzbuzz/csharp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int n = 1; n <= 100; n++)
+            {
+                if (n % 15 == 0)
+                {
+                    Console.Write("fizzbuzz");
+                }
+                else if (n % 3 == 0)
+                {
+                    Console.Write("fizz");
+                }
+                else if (n % 5 == 0)
+                {
+                    Console.Write("buzz");
+                }
+                else
+                {
+                    Console.Write(n);
+                }
+            }
+        }
+    }
+}
