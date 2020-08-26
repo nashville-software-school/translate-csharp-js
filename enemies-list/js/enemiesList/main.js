3dd976a691f91a68cb73ae50023ddf5318860a00
Lacey Walker <bluelarksong@gmail.com>
2020-08-26 00:20:45 -0500

    changed out = with :, then rearranged code so the for loop works

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -6,6 +6,50 @@ function main() {
   console.log("My Enemies List!");
   console.log("----------------");
 
+  const enemies = [
+    {
+      firstName: "Joshua",
+      lastName: "Flowers",
+      offences: ["Being a jerk to me in elementary school",
+        "Not being nice to me in elementary school"],
+      isReallyHated: true
+    },
+
+    {
+      firstName: "Darth",
+      lastName: "Vader",
+      offences: ["Cut off luke's hand",
+        "Murdered all those kids",
+        "Unkind management practices"],
+      isReallyHated: false
+    },
+
+    {
+      firstName: "Betty",
+      lastName: "McRudeLady",
+      offences: ["Phone calls in the theater",
+        "Phone calls on the bus",
+        "Phone calls in line at the grocery store",
+        "Poor conversationalist"],
+      isReallyHated: true
+    },
+
+    {
+      firstName: "Leon",
+      lastName: "Peck",
+      offences: ["Keeps giving me a hotplate"],
+      isReallyHated: false
+    },
+
+    {
+      firstName: "Karen",
+      lastName: "Smith",
+      offences: ["Always asks to speak to the manager",
+        "extremely entitled"],
+      isReallyHated: true
+    }
+  ]
+
   for (let enemy of enemies) {
     if (enemy.isReallyHated === true) {
       console.log(`${enemy.firstName} ${enemy.lastName} really really displike! EW.`);
@@ -14,48 +58,5 @@ function main() {
     }
   }
 
-  const enemies = [
-    {
-      firstName = "Joshua",
-      lastName = "Flowers",
-      offences =["Being a jerk to me in elementary school",
-        "Not being nice to me in elementary school"],
-      isReallyHated: true
-    },
-
-    {
-      firstName = "Darth",
-      lastName = "Vader",
-      offences =["Cut off luke's hand",
-        "Murdered all those kids",
-        "Unkind management practices"],
-      isReallyHated: false
-    },
-
-    {
-      firstName = "Betty",
-      lastName = "McRudeLady",
-      offences =["Phone calls in the theater",
-        "Phone calls on the bus",
-        "Phone calls in line at the grocery store",
-        "Poor conversationalist"],
-      isReallyHated: true
-    },
-
-    {
-      firstName = "Leon",
-      lastName = "Peck",
-      offences =["Keeps giving me a hotplate"],
-      isReallyHated: false
-    },
-
-    {
-      firstName = "Karen",
-      lastName = "Smith",
-      offences =["Always asks to speak to the manager",
-        "extremely entitled"],
-      isReallyHated: true
-    }
-  ]
-
-  main();
+}
+main();
