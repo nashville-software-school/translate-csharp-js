4cf7e3add0e7f175c3201f06c70fd406c822b684
Lacey Walker <bluelarksong@gmail.com>
2020-08-26 00:37:26 -0500

    started on rolling dice, but im sleepy

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,6 +1,17 @@
 function main() {
   // Put your code here
 
+  console.log("Let's roll some dice, baby!");
+  console.log("---------------------------")
+
+  for (let i = 0; i < 10; i++) {
+    console.log(`The number is ${i}`);
+  }
+
+  // const die1 
+  // const die2
+
+  // let message
 }
 
 main();
