5f70f3f47aa4d48674b5112cad52977cf667b20d
Kelley Crittenden <kelleylcrittenden@gmail.com>
2020-09-03 10:12:47 -0500

    Part 1 & 2 Complete

--- insulter/csharp/Insulter/Program.cs
+++ insulter/csharp/Insulter/Program.cs
@@ -8,7 +8,7 @@ namespace Insulter
     {
         static void Main(string[] args)
         {
-            List<string> insults = new List<string>
+            List<string> insults = new List<string>()
             {
                 "You look like what morning breath smells like.",
                 "If you tried to give me cpr I would probably throw myself back under water",
@@ -38,6 +38,25 @@ namespace Insulter
                 int index = RandomInsultsList[i];
                 Console.WriteLine(insults[index]);
             }
+
+            //Andy's Demo Code
+
+            // List<int> indexes = new List<int>();
+
+            // while (indexes.Count < 3)
+            // {
+            //     int candidate = new Random().Next(0, insults.Count);
+            //     if (!indexes.Contains(candidate))
+            //     {
+            //         indexes.Add(candidate);
+            //     }
+            // }
+
+            // for (int i = 0; i < indexes.Count; i++)
+            // {
+            //     int index = indexes[i];
+            //     Console.WriteLine(insults[index]);
+            // }
         }
     }
 }
