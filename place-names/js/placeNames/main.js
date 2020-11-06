3abbc3868f59349ba64ea58ce6a941fdda11528c
Lacey Walker <bluelarksong@gmail.com>
2020-11-05 20:40:21 -0600

    started this up again

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,6 +1,13 @@
 function main() {
   // Put your code here
+  const names = [
+    "Nashville", "Hong Kong", "The back yard", "Earth",
+    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
+  ]
+  console.log(names)
 
+  const theNames = names.filter(name => name.startsWith("The"))
+  console.log(theNames)
 }
 
 main();
