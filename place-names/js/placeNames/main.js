9a33fa243d781d4a214d5c5ef356746c0a1cdbbd
jenniferstarkey <jenniferrstarkey@yahoo.com>
2020-11-13 13:35:04 -0600

    place names in javascript

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,5 +1,26 @@
+//names: "Nashville", "Hong Kong", "The back yard", "Earth",
+// "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
+
 function main() {
   // Put your code here
+  const Names = [
+    "Nashville",
+    "Hong Kong",
+    "The back yard",
+    "Earth",
+    "London",
+    "The mall",
+    "Ryman Auditorium",
+    "The Great Wall of China"
+  ];
+
+  console.log("Names")
+  for (const name of Names)
+    console.log(name)
+
+  const theNames = Names.filter(names => names.startsWith("The"))
+  console.log("The names")
+  console.log(theNames)
 
 }
 
