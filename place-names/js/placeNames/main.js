e3e399f434ca2aec63f4a7baca131047b71ad1b9
Patchrik <pklly92@gmail.com>
2020-11-13 15:00:48 -0600

    Finish Place Names - on to the next one

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,6 +1,25 @@
 function main() {
   // Put your code here
+  const names = [
+    'Nashville',
+    'Hong Kong',
+    'The back yard',
+    'Earth',
+    'London',
+    'The mall',
+    'Ryman Auditorium',
+    'The Great Wall of China',
+  ];
 
+  names.forEach((name) => console.log(name));
+
+  // Need to add a new line.
+  console.log('\n');
+
+  // Filter the THE names into a new array to print.
+  const theNames = names.filter((name) => name.includes('The'));
+
+  theNames.forEach((name) => console.log(name));
 }
 
 main();
