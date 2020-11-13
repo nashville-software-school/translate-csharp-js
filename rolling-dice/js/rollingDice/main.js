866d7edc791958419d7c09256aaea971daeb675e
jenniferstarkey <jenniferrstarkey@yahoo.com>
2020-11-13 15:29:56 -0600

    dice

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,6 +1,26 @@
 function main() {
   // Put your code here
+  console.log("Let's roll some dice, baby!");
+  console.log("---------------------------");
+
+
+  for (let i = 0; i < 10; i++) {
+    let die1 = dieRoll();
+    let die2 = dieRoll();
+
+    let message = `${die1} + ${die2} = ${die1 + die2}`;
+    if (die1 === die2) {
+      message += " DOUBLES!";
+    }
+
+    console.log(message);
+  }
 
 }
+
+const dieRoll = () => {
+  return parseInt(Math.random() * (7 - 1) + 1);
+}
+
 
 main();
