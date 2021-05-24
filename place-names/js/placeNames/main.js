9062edeea7c5370c3b74088e5b365ee95b30970c
dudley-codes <alexdudley@me.com>
2021-05-24 11:47:01 -0500

    console logged place names in JS

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,1 +1,8 @@
 // Put your code here
+const names = [ "Nashville", "Hong Kong", "The back yard", "Earth", "London", "The mall", "Ryman Auditorium", "The Great Wall of China" ];
+
+console.log("All Place Names");
+
+for (eachName of names) {
+  console.log(eachName);
+}
