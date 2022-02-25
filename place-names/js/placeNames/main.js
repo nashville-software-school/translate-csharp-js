100267faa452ce518fde537905ee22d7ae7fcb8f
Jennifer Williams <jennifer.blue.williams@gmail.com>
2022-02-25 10:44:35 -0600

    Filtered C# translated into JavaScript to return places beginning with The

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,12 +1,27 @@
 // Put your code here
 
 const names = [
-   "Nashville", "Hong Kong", "The back yard", "Earth",
-    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
+  "Nashville",
+  "Hong Kong",
+  "The back yard",
+  "Earth",
+  "London",
+  "The mall",
+  "Ryman Auditorium",
+  "The Great Wall of China",
 ];
 
 console.log("All Pace Names");
 
 for (const name of names) {
   console.log(name);
+}
+
+const theNames = names.filter((name) => {
+  return name.startsWith("The");
+});
+console.log(theNames);
+
+for (const theName of theNames) {
+  console.log(theName);
 }
